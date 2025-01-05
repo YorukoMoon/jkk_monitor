28308ed098f9e4b4e223fc46227edb9f9bd5daa6
CHENXING <g1465422485@gmail.com>
2025-01-05 23:14:34 +0900

    邮件测试文件sendmailTest成功发送！

--- sendemailTest.js
+++ sendemailTest.js
@@ -4,15 +4,15 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail',  // 这里使用 Gmail 的 SMTP 服务，其他邮件服务商可以使用相应的设置
     auth: {
-        user: 'your-email@gmail.com',  // 发件人邮箱地址
-        pass: 'your-email-password',   // 发件人邮箱密码
+        user: 'g1465422485@gmail.com',  // 发件人邮箱地址
+        pass: 'urtqwnhtqcutxbto',   // 发件人邮箱密码
     },
 });
 
 // 设置邮件内容
 let mailOptions = {
-    from: 'your-email@gmail.com',  // 发件人邮箱地址
-    to: 'recipient-email@example.com',  // 收件人邮箱地址
+    from: 'g1465422485@gmail.com',  // 发件人邮箱地址
+    to: 'g1465422485@outlook.com',  // 收件人邮箱地址
     subject: 'Test Email',  // 邮件主题
     text: 'This is a test email sent from Node.js using nodemailer.',  // 邮件正文
 };
