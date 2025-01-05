365a816fcfa412551bdf40bb8e2f209c674a9c08
CHENXING <g1465422485@gmail.com>
2025-01-05 23:04:14 +0900

    获取房源已经全部完成，没有问题，开始实现发邮件功能

--- kumegawa.mjs
+++ kumegawa.mjs
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
+
 
 const url = "https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenRef";
 let token = ""; // 用于存储 token
@@ -33,7 +35,7 @@ const formData = new URLSearchParams({
     "akiyaInitRM.akiyaRefM.yuguFlg": "",
     "akiyaInitRM.akiyaRefM.ekiCd": ""
 });
-const headers = {
+const headers = new Headers({
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6",
@@ -53,22 +55,22 @@ const headers = {
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-};
-//定义函数，获取久米川房源情况
-function kumegawaFetch() {
-    fetch(url, {
-        method: "POST",
-        headers: headers,
-        body: formData.toString()
-    }).then(response => response.arrayBuffer())//获取二进制的response以便解码
-        .then(buffer => {
-            // 使用 shift_jis 解码（Windows-31J 兼容）
-            const decoder = new TextDecoder('shift_jis');
-            const text = decoder.decode(buffer);
-            console.log("解码后的响应:", text);
-        })
-        .catch(error => console.error("请求出错:", error));
-}
+});
+// //定义函数，获取久米川房源情况
+// function kumegawaFetch() {
+//     fetch(url, {
+//         method: "POST",
+//         headers: headers,
+//         body: formData.toString()
+//     }).then(response => response.arrayBuffer())//获取二进制的response以便解码
+//         .then(buffer => {
+//             // 使用 shift_jis 解码（Windows-31J 兼容）
+//             const decoder = new TextDecoder('shift_jis');
+//             const text = decoder.decode(buffer);
+//             console.log("解码后的响应:", text);
+//         })
+//         .catch(error => console.error("请求出错:", error));
+// }
 
 // 每 10 秒发送一次请求获取房源
 async function fetchData() {
@@ -90,7 +92,18 @@ async function fetchData() {
             // 使用 shift_jis 解码（Windows-31J 兼容）
             const decoder = new TextDecoder('shift_jis');
             const text = decoder.decode(buffer);
-            console.log("解码后的响应:", text);
+            const matchKumegawa = text.match(/久米川駅東/);
+            if (matchKumegawa) {
+                console.log("房源请求成功:" + "本次结果包含久米川駅東！！！!");
+                //关闭所有定时器
+                clearInterval(intervalToken);
+                clearInterval(intervalGetHouse);
+                //开启每分钟发送一次邮件的定时器
+                sendEmail();
+                intervalSendEmail = setInterval(sendEmail, 60 * 1000);//每分钟执行一次
+            }
+            else
+                console.log("房源请求成功:" + "本次结果不包含久米川駅東.....");
         } else {
             console.error("房源请求失败");
         }
@@ -100,50 +113,60 @@ async function fetchData() {
 }
 
 
-//定义函数，刷新token
-function tokenFetch() {
-    console.log(1);
-    fetch('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit', {
-        method: 'POST',
-        headers: {
-            'Host': 'jhomes.to-kousya.or.jp',
-            'Connection': 'keep-alive',
-            'Content-Length': '106',
-            'Cache-Control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'Origin': 'https://jhomes.to-kousya.or.jp',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Dest': 'document',
-            'Referer': 'https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6',
-            'Cookie': 'koushinflg=1; JSESSIONID=C2D05uwXTZarhOrKATq-sbGR.112M7MF'
-        },
-        body: 'redirect=true&url=https%3A%2F%2Fjhomes.to-kousya.or.jp%2Fsearch%2Fjkknet%2Fservice%2FakiyaJyoukenStartInit'
-    }).then(response => response.text()) // 获取文本形式的响应
-        .then(data => {
-            //对响应正则匹配，提取token
-            const match = data.match(/name=token value="([^"]+)"/);
-            if (match) {
-                const tokenValue = match[1];
-                console.log("提取到的 token:", tokenValue);
-                //给表单数据赋值token
-                formData.set("token", tokenValue);
-            } else {
-                console.log("未找到 token");
-            }
-        })
-        .catch(error => console.error("请求出错:", error));
-}
+// //定义函数，刷新token
+// function tokenFetch() {
+//     console.log(1);
+//     fetch('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit', {
+//         method: 'POST',
+//         headers: {
+//             'Host': 'jhomes.to-kousya.or.jp',
+//             'Connection': 'keep-alive',
+//             'Content-Length': '106',
+//             'Cache-Control': 'max-age=0',
+//             'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+//             'sec-ch-ua-mobile': '?0',
+//             'sec-ch-ua-platform': '"Windows"',
+//             'Origin': 'https://jhomes.to-kousya.or.jp',
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             'Upgrade-Insecure-Requests': '1',
+//             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+//             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+//             'Sec-Fetch-Site': 'same-origin',
+//             'Sec-Fetch-Mode': 'navigate',
+//             'Sec-Fetch-Dest': 'document',
+//             'Referer': 'https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit',
+//             'Accept-Encoding': 'gzip, deflate, br, zstd',
+//             'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6',
+//             'Cookie': 'koushinflg=1; JSESSIONID=C2D05uwXTZarhOrKATq-sbGR.112M7MF'
+//         },
+//         body: 'redirect=true&url=https%3A%2F%2Fjhomes.to-kousya.or.jp%2Fsearch%2Fjkknet%2Fservice%2FakiyaJyoukenStartInit'
+//     }).then(response => response.text()) // 获取文本形式的响应
+//         .then(data => {
+//             //对响应正则匹配，提取token，cookie
+//             const matchToken = data.match(/name=token value="([^"]+)"/);
+//             if (match) {
+//                 const tokenValue = matchToken[1];
+//                 console.log("提取到的 token:", tokenValue);
+//                 //给表单数据赋值token
+//                 formData.set("token", tokenValue);
+//             } else {
+//                 console.log("未找到 token");
+//             }
 
-//每 10 分钟获取一次 token
+//             const matchCookie = data.match(/name=token value="([^"]+)"/);
+//             if (match) {
+//                 const cookie = matchCookie[1];
+//                 console.log("提取到的cookie:", cookie);
+//                 //给表单数据赋值cookie
+//                 formData.set("cookie", cookie);
+//             } else {
+//                 console.log("未找到cookie");
+//             }
+//         })
+//         .catch(error => console.error("请求出错:", error));
+// }
+
+//每 10 分钟获取一次token，cookie
 async function fetchToken() {
     try {
         const response = await fetch("https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit", {
@@ -167,37 +190,59 @@ async function fetchToken() {
                 'Referer': 'https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit',
                 'Accept-Encoding': 'gzip, deflate, br, zstd',
                 'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6',
-                'Cookie': 'koushinflg=1; JSESSIONID=C2D05uwXTZarhOrKATq-sbGR.112M7MF'
+                //自行填充，格式为：JSESSIONID=Cookie内容
+                'Cookie': ''
             },
             body: 'redirect=true&url=https%3A%2F%2Fjhomes.to-kousya.or.jp%2Fsearch%2Fjkknet%2Fservice%2FakiyaJyoukenStartInit'
         });
 
         if (response) {
             const data = await response.text();
+            // const content = "这是要写入文件的内容";
+            // // 写入文件（会覆盖已有内容）
+            // fs.writeFileSync('tempJsOutput.txt', response.headers.toString(), 'utf8');
+            // console.log('文件写入成功');
+            // console.log(response.headers.get("Set-Cookie"));
+
             //对响应正则匹配，提取token
-            const match = data.match(/name=token value="([^"]+)"/);
-            if (match) {
-                token = match[1];
-                console.log("提取到的 token:", token);
+            const matchToken = data.match(/name=token value="([^"]+)"/);
+            //提取Set-Cookie: JSESSIONID=RR2Z7qe33zhGvybmFWamlZsR.183E7T6;
+            const matchCookie = response.headers.get("Set-Cookie").match(/([^;]+);/);
+            if (matchToken) {
+                token = matchToken[1];
+                console.log("提取到的token:", token);
                 //给表单数据赋值token
                 formData.set("token", token);
             } else {
-                console.log("未找到 token");
+                console.log("未找到token");
+            }
+            if (matchCookie) {
+                const cookie = matchCookie[1];
+                console.log("提取到的cookie:", cookie);
+                //给数据赋值cookie
+                headers.set("cookie", "koushinflg=0; " + cookie);
+            } else {
+                console.log("未找到cookie");
             }
         } else {
-            console.error("获取 token 失败");
+            console.error("获取 cookie 失败");
         }
-        console.log('token刷新完毕');
+        console.log('token,cookie刷新完毕');
     } catch (error) {
-        console.error("请求 token 时出错:", error);
+        console.error("请求token,cookie时出错:", error);
     }
 }
+//定义函数，发送邮件
+function sendEmail() {
+    
+}
+let intervalToken;
+let intervalGetHouse;
+let intervalSendEmail;
 
-console.log(2);
-
- // 启动token请求，每 10 分钟获取一次 token
+// 启动token请求，每 10 分钟获取一次 token
 fetchToken(); // 立即执行一次
-setInterval(fetchToken, 10 * 60 * 1000); // 每 10 分钟执行一次
+intervalToken = setInterval(fetchToken, 10 * 60 * 1000); // 每 10 分钟执行一次
 
-// 启动房源请求，每 10 秒使用最新的 token 请求
-setInterval(fetchData, 10 * 1000); // 每 10 秒执行一次
+// 启动房源请求，每请求一次（使用最新的token，cookie）
+intervalGetHouse = setInterval(fetchData, 10 * 1000); // 每 10 秒执行一次
